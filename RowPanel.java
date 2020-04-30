1f17baf13322ddc71a1cd1176bc0ac16942a01f5
VictoriaTrenton <56134419+VictoriaTrenton@users.noreply.github.com>
2020-04-30 04:15:41 -0400

    Update RowPanel.java

--- RowPanel.java
+++ RowPanel.java
@@ -1,7 +1,6 @@
 //******************************************************************************
 // 		RowPanel.java
-//									Authors:   Partner &
-//											   Vicki Trenton
+//									Authors:   Partner & Victoria Trenton
 //									Class:	   CS152
 //									Assignment:Texas Hold'em Game
 //
